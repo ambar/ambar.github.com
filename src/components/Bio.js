7039478c4dcd87ed381f7b88bc3b23060ddf9c42
ambar <ambar.lee@gmail.com>
2018-03-14 21:03:30 +0800

    init blog

---
+++ src/components/Bio.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import {rhythm} from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <p />
+      </div>
+    )
+  }
+}
+
+export default Bio

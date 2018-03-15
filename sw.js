b7c011e9ced78fed51deb8a7d2b630142601e65e
ambar <ambar.lee@gmail.com>
2018-03-15 09:02:41 +0800

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-93cf1df3b4cb5fb26ed6.js","85560f785b154236f84e83a9eb516b26"],["/commons-bde6322439ae79794142.js","09e58c4c6d0dcc996ca9c92f0892f657"],["/index.html","3244701c46d9d429754d4da230318161"],["/offline-plugin-app-shell-fallback/index.html","f8dd8fc9e09b84928cdb9e41f525a90e"]];
+var precacheConfig = [["/app-79a513ff8922b0523372.js","7c1e411e9e6bf8cf7d29ed5e1461b309"],["/commons-bde6322439ae79794142.js","a72f0d2a5bfcc7b0d1b8e92c210bc94a"],["/index.html","232504819d421d13db44f65735896545"],["/offline-plugin-app-shell-fallback/index.html","b3ee17f0b9fa926ecbb730379c36c221"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
